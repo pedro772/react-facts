07129532ce7a47486855c638307f87549e45383e
pedro772 <plucena17230@gmail.com>
2022-02-02 16:29:05 -0300

    Making it public

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useState } from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode(prevTheme => !prevTheme);
+  }
+
+    return (
+        <div className="container">
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
